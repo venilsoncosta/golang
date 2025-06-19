a40a2160b993eea5150a940bbc4dbbd6f4d04266
Venilson Costa Neves <venilsoncosta21@gmail.com>
2025-06-19 09:10:15 -0300

    entendendo pacotes e imports

---
+++ pacotes/calculos/matematicos.go
@@ -0,0 +1,9 @@
+package calculos
+
+func Soma(a, b int) int {
+	return a + b
+}
+
+func Subtrai(a, b int) int {
+	return a - b
+}
---
+++ pacotes/pacotes.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"go-gemini/pacotes/calculos"
+)
+
+func main() {
+	resultadoSoma := calculos.Soma(10, 5)
+	fmt.Println(resultadoSoma)
+}
